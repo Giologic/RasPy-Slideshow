cb050118ed44b66c78e61ce0ed61741ce823798d
Clarissa <41070171+cmmagsipoc16@users.noreply.github.com>
2019-11-08 12:37:25 +0800

    Register device even when device_status.json hasn't been created yet because it's the first time

--- slideshow.py
+++ slideshow.py
@@ -229,8 +229,8 @@ class SlideShowApp(object):
 
         try:
             self.check_device_status() # ensures self.check_device_name exists-or-not.
-            print("Not Device Status:", self.device_status, "and Name from Check_device_status:", self.check_device_status())
-            if os.path.exists('.env') and self.device_status == False and self.check_device_name == None:#and not self.check_device_status():
+            print("Device Status:", self.device_status, "and Name from Check_device_status:", self.check_device_status())
+            if os.path.exists('.env') and (self.device_status == False or self.device_status == None) and self.check_device_name == None:#and not self.check_device_status():
                 response = requests.post(
                     ADTECH_ENDPOINT + '/devices', 
                     data={'deviceUid': config('deviceUid', cast=str), 
